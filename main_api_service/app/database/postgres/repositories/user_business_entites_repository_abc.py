85d6d107d0aaedd031aa7e8589c8721c53d2bafc
Wojciech <84650124+wojciechignasiak@users.noreply.github.com>
2023-11-27 10:11:27 +0100

    IWB-79 update user business entity method implemented

--- main_api_service/app/database/postgres/repositories/user_business_entites_repository_abc.py
+++ main_api_service/app/database/postgres/repositories/user_business_entites_repository_abc.py
@@ -1,8 +1,15 @@
 from abc import ABC, abstractmethod
 from app.schema.schema import UserBusinessEntity
-from app.models.user_business_entity_model import CreateUserBusinessEntityModel
+from app.models.user_business_entity_model import (
+    CreateUserBusinessEntityModel,
+    UpdateUserBusinessEntityModel
+)
 
 class UserBusinessEntityRepositoryABC(ABC):
     @abstractmethod
-    async def create_user_business_entity(self, user_id: str, new_business: CreateUserBusinessEntityModel) -> UserBusinessEntity:
+    async def create_user_business_entity(self, user_id: str, new_user_business_entity: CreateUserBusinessEntityModel) -> UserBusinessEntity:
+        pass
+
+    @abstractmethod
+    async def update_user_business_entity(self, update_user_business_entity: UpdateUserBusinessEntityModel) -> UserBusinessEntity:
         pass
