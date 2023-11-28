4b31c0abb4cfcceeabfa77b74fbdefbc53d55c8b
Wojciech <84650124+wojciechignasiak@users.noreply.github.com>
2023-11-28 13:32:45 +0100

    IWB-89 remove user business entity event implemented (#37)

---
+++ main_api_service/app/kafka/events/user_business_entity_events.py
@@ -0,0 +1,26 @@
+from aiokafka import AIOKafkaProducer
+from aiokafka.errors import KafkaError
+from app.kafka.exceptions.custom_kafka_exceptions import KafkaBaseError
+from app.models.kafka_topics_enum import KafkaTopicsEnum
+import json
+from app.logging import logger
+
+class UserBusinessEntityEvents:
+
+    def __init__(self, kafka_producer: AIOKafkaProducer):
+        self.kafka_producer: AIOKafkaProducer = kafka_producer
+
+    async def remove_user_business_entity(self, id: str, email_address: str, user_business_entity_name: str):
+        try:
+            message = {
+                "id": id, 
+                "email": email_address,
+                "user_business_entity_name": user_business_entity_name
+            }
+            await self.kafka_producer.send(
+                KafkaTopicsEnum.remove_user_business_entity.value, 
+                json.dumps(message).encode('utf-8')
+                )
+        except KafkaError as e:
+            logger.exception(f"UserBusinessEntityEvents.remove_user_business_entity() Error: {e}")
+            raise KafkaBaseError("Error related to Kafka occured.")
