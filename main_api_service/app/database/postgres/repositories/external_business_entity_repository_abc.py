74690558392b1465396dd2d58a68e0aa4132e62c
Wojciech <84650124+wojciechignasiak@users.noreply.github.com>
2023-12-05 14:21:50 +0100

    IWB-96 is unique external business entity implemented

--- main_api_service/app/database/postgres/repositories/external_business_entity_repository_abc.py
+++ main_api_service/app/database/postgres/repositories/external_business_entity_repository_abc.py
@@ -9,3 +9,7 @@ class ExternalBusinessEntityPostgresRepositoryABC(ABC):
     @abstractmethod
     async def create_external_business_entity(self, user_id: str, new_external_business_entity: CreateExternalBusinessEntityModel) -> ExternalBusinessEntity:
         pass
+
+    @abstractmethod
+    async def is_external_business_entity_unique(self, user_id: str, new_external_business_entity: CreateExternalBusinessEntityModel) -> bool:
+        pass
