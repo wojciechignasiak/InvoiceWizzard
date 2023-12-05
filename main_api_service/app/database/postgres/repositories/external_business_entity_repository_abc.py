f376002e3e871d46109e470d5440c1bc1a4e553c
Wojciech <84650124+wojciechignasiak@users.noreply.github.com>
2023-12-05 14:33:04 +0100

    IWB-93 update external business entity method implemented

--- main_api_service/app/database/postgres/repositories/external_business_entity_repository_abc.py
+++ main_api_service/app/database/postgres/repositories/external_business_entity_repository_abc.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
 from app.schema.schema import ExternalBusinessEntity
 from app.models.external_business_entity_model import (
-    CreateExternalBusinessEntityModel
+    CreateExternalBusinessEntityModel,
+    UpdateExternalBusinessEntityModel
 )
 
 class ExternalBusinessEntityPostgresRepositoryABC(ABC):
@@ -12,4 +13,8 @@ class ExternalBusinessEntityPostgresRepositoryABC(ABC):
 
     @abstractmethod
     async def is_external_business_entity_unique(self, user_id: str, new_external_business_entity: CreateExternalBusinessEntityModel) -> bool:
+        pass
+
+    @abstractmethod
+    async def update_external_business_entity(self, user_id: str, update_external_business_entity: UpdateExternalBusinessEntityModel) -> ExternalBusinessEntity:
         pass
